ebd71bdf999b6442fc492bf3323d4610bc124bc4
sanjayskv1 <sanjaysksk6143@gmail.com>
2024-07-06 21:02:43 -0700

    Add files via upload

---
+++ My Life/HTML/script.js
@@ -0,0 +1,15 @@
+var sidenavbar = document.querySelector(".side-navbar")
+
+function showbar(){
+    sidenavbar.style.left="0"
+}
+
+function closebar(){
+    sidenavbar.style.left="-50%"
+}
+var insta = document.getElementById("gram")
+
+function click(event){
+    
+     event.target.URL="https://www.instagram.com/vaalu_paiyan_sk__?igsh=MTcyZWQ1aHZ3MnU0dw=="
+}
